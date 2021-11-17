f4b544820eeb07778e6f88c28afacf3190c19bc2
BabSangJune <applause1319@naver.com>
2021-11-18 02:15:38 +0900

    2021_11_18-mainpage_index
    mainpage / index로 설정
    background 완성

--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    
+  <div id="main">
+    <h1>Main Page</h1>
+
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Main',
+  components: {
 
+  },
 }
 </script>
 
---
+++ client/src/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <a href="http://localhost:8080/main/" style="z-index: 1;">
+      <div class="main-img d-flex justify-center">
+      <v-img 
+        max-width="75%" height="70%" contain="true"
+        src="@/assets/main_test.jpg"
+        ></v-img>
+    </div></a>
+  </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
