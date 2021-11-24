1282696e906ba30d06aa5092e977782fbd6f4af5
BabSangJune <applause1319@naver.com>
2021-11-24 09:16:49 +0900

    Update Main.vue

--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="main" class="container">
+    <div class="welcome">
+      <p>Recommend for you</p>
+    </div>
     <carousel id="carousel-slide"></carousel>
 
     <!-- <v-container>
@@ -13,18 +16,21 @@
           <v-img class="main-img" width= "50%" src="@/assets/main_test.jpg"></v-img>
         </v-col>
       </v-row>
-    </v-container> -->
+    </v-container>
+
+
+
     <div>
       <div class="d-flex justify-content-around">
         <img class="main-img" src="@/assets/main_test.jpg" alt="">
         <img class="main-img" src="@/assets/main_test.jpg" alt="">
       </div>
-      <!--<div>
+      <div>
         <img class="main-img" src="@/assets/main_test.jpg" alt="">
         <img class="main-img" src="@/assets/main_test.jpg" alt="">
-      </div>-->
-    </div>
-  </div>
+      </div>
+    </div>-->
+  </div> 
 </template>
 
 <script>
@@ -45,6 +51,43 @@ export default {
 
   #carousel-slide{
     z-index: 5;
+  }
+
+  .welcome {
+    z-index: 999;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .welcome p {
+    position: relative;
+    font-family: sans-serif;
+    /* color: white; */
+    text-transform: uppercase;
+    font-size: 2em;
+    letter-spacing: 4px;
+    overflow: hidden;
+    background: linear-gradient(90deg, #000, #fff, #000);
+    background-repeat: no-repeat;
+    background-size: 70%;
+    animation: animate 4s linear infinite;
+    -webkit-text-fill-color: rgba(255, 255, 255, 0);
+    -webkit-background-clip: text;
+  }
+
+  @keyframes animate
+  {
+    0%
+    {
+      background-position: -400%;
+    }
+    100%
+    {
+      background-position: 300%;
+    }
   }
 
   .main-img {
